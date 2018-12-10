75592c1361975d1045997e1100f4b963d5639d6f
Alexander Gromov <i2010.grom@yandex.ru>
2018-12-10 13:56:51 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# GoZix GoRedis
+
+## Dependencies
+
+* [viper](https://github.com/gozix/viper)
+
+## Configuration example
+
+```json
+{
+  "redis": {
+    "host": "127.0.0.1",
+    "port": "6379",
+    "read_timeout": "2s",
+    "write_timeout": "2s",
+    "idle_timeout": "1m"
+  }
+}
+```
---
+++ goredis.go
@@ -0,0 +1,91 @@
+// Package goredis provides implementation of go-redis client.
+package goredis
+
+import (
+	"errors"
+	"net"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/gozix/viper"
+	"github.com/sarulabs/di"
+)
+
+type (
+	// Bundle implements the glue.Bundle interface.
+	Bundle struct{}
+
+	// Pool is type alias of redis.Client
+	Pool = redis.Client
+
+	// redisConf is logger configuration struct.
+	redisConf struct {
+		Host         string
+		Port         string
+		IdleTimeout  time.Duration `mapstructure:"idle_timeout"`
+		ReadTimeout  time.Duration `mapstructure:"read_timeout"`
+		WriteTimeout time.Duration `mapstructure:"write_timeout"`
+	}
+)
+
+// BundleName is default definition name.
+const BundleName = "redis"
+
+// NewBundle create bundle instance.
+func NewBundle() *Bundle {
+	return new(Bundle)
+}
+
+// Name implements the glue.Bundle interface.
+func (b *Bundle) Name() string {
+	return BundleName
+}
+
+// Build implements the glue.Bundle interface.
+func (b *Bundle) Build(builder *di.Builder) error {
+	return builder.Add(di.Def{
+		Name: BundleName,
+		Build: func(ctn di.Container) (_ interface{}, err error) {
+			var cnf *viper.Viper
+			if err = ctn.Fill(viper.BundleName, &cnf); err != nil {
+				return nil, errors.New("can't get config from container")
+			}
+
+			var conf redisConf
+			if err = cnf.UnmarshalKey("redis", &conf); err != nil {
+				return nil, err
+			}
+
+			options := &redis.Options{
+				Addr: net.JoinHostPort(
+					conf.Host,
+					conf.Port,
+				),
+				IdleTimeout:  conf.IdleTimeout,
+				ReadTimeout:  conf.ReadTimeout,
+				WriteTimeout: conf.WriteTimeout,
+			}
+
+			var client *redis.Client
+			if client = redis.NewClient(options); client == nil {
+				return nil, err
+			}
+
+			if _, err = client.Ping().Result(); err != nil {
+				return nil, err
+			}
+
+			return client, nil
+		},
+		Close: func(obj interface{}) error {
+			return obj.(*redis.Client).Close()
+		},
+	})
+}
+
+// DependsOn implements the glue.DependsOn interface.
+func (b *Bundle) DependsOn() []string {
+	return []string{
+		viper.BundleName,
+	}
+}
